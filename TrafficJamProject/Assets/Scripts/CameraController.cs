02ce00de7ff83e3272ce196d7d0576f79ceccb14
NuiN99 <endyverse@gmail.com>
2023-10-01 18:35:58 -0700

    decent cam movement

--- TrafficJamProject/Assets/Scripts/CameraController.cs
+++ TrafficJamProject/Assets/Scripts/CameraController.cs
@@ -4,23 +4,51 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
+    Camera cam;
     Transform player;
     public float verticalOffset;
     ScreenShake sc;
 
+    [SerializeField] float smoothSpeed;
+
     public Vector3 targetPos;
+
+    [SerializeField] float minCamSize;
+    [SerializeField] float maxCamSize;
+    [SerializeField] float cameraScaleExponent;
+
+    [SerializeField] float speedFactor;
 
     private void Awake()
     {
+        cam = Camera.main;
         sc = GetComponent<ScreenShake>();
         player = FindObjectOfType<Player>(true).transform;   
     }
 
     private void LateUpdate()
     {
-        targetPos = player.position + Vector3.up * verticalOffset;
+        //normal smoothing
+        /*Vector3 desiredPos = player.position + new Vector3(0, verticalOffset);
+        targetPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);*/
 
-        if(!sc.shaking)
+        //bad scaling code
+        /*if(player.TryGetComponent(out Rigidbody2D playerRB))
+        {
+            float speedFactor = playerRB.velocity.magnitude / maxCamSize;
+            float lerpFactor = Mathf.Pow(speedFactor, cameraScaleExponent);
+
+            cam.orthographicSize = Mathf.Lerp(minCamSize, maxCamSize, lerpFactor);
+            cam.orthographicSize = Mathf.Clamp(cam.orthographicSize, minCamSize, maxCamSize);
+        }*/
+
+
+        Rigidbody2D playerRigidbody = player.GetComponent<Rigidbody2D>();
+        float verticalOffsetTemp = playerRigidbody.velocity.y * speedFactor + verticalOffset;
+        Vector3 desiredPos = player.position + new Vector3(0, verticalOffsetTemp);
+        targetPos = Vector3.Lerp(transform.position, desiredPos, smoothSpeed * Time.deltaTime);
+
+        if (!sc.shaking)
             transform.position = new Vector3(targetPos.x, targetPos.y, transform.position.z);
     }
 }
