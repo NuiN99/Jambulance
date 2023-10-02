24d21e1b79448dc8fa3d93a4e7b566626be6ad42
NuiN99 <endyverse@gmail.com>
2023-10-01 22:54:47 -0700

    added time remaining text

--- TrafficJamProject/Assets/Scripts/GameController.cs
+++ TrafficJamProject/Assets/Scripts/GameController.cs
@@ -12,9 +12,25 @@ public class GameController : MonoBehaviour
     public float distToEnd;
     public float progress01;
 
+    public float startYPos;
+
+    [SerializeField] float startTime = 90f;
+    public float timeRemaining;
+
+    public static GameController Instance;
 
     private void Awake()
     {
+        if(Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+
         Physics2D.queriesStartInColliders = false;
         player = FindObjectOfType<Player>();
     }
@@ -23,10 +39,21 @@ public class GameController : MonoBehaviour
     {
         player.enabled = false;
         player.AddComponent<Enemy>();
+
+        timeRemaining = startTime;
     }
 
     private void Update()
     {
+        if (started)
+        {
+            timeRemaining -= Time.deltaTime;
+            if(timeRemaining <= 0)
+            {
+                timeRemaining = 0;
+                print("RAN OUT OF TIME");
+            }
+        }
         if (!started && Input.anyKeyDown)
         {
             started = true;
@@ -34,6 +61,9 @@ public class GameController : MonoBehaviour
             carsController.SwitchStats(player.GetComponent<Car>(), carsController.playerStats);
             Destroy(player.GetComponent<Enemy>());
             player.enabled = true;
+
+            startYPos = player.transform.position.y;
+            endPosY += startYPos;
         }
     }
 
@@ -42,6 +72,11 @@ public class GameController : MonoBehaviour
         distToEnd = endPosY - player.transform.position.y;
         progress01 = 1 - (distToEnd / endPosY);
         progress01 = Mathf.Clamp01(progress01);
+
+        if(progress01 >= 1)
+        {
+            print("YOU WON GOOD JOB");
+        }
     }
 }
 
